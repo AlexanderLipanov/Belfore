7ca28fa0ee18e0e2495fb1b68965ec73c4250e1b
Alexandr <lipanov162@gmail.com>
2023-03-06 08:31:07 +0300

    show menu, new func

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,13 @@
 
-
+const px = "px";
 let burgerLinksContainer = document.getElementById("burger-links-container");
 function toggleLinksContainer() {
-    burgerLinksContainer.classList.toggle("show");
+    let burgerLinksList = document.getElementById("burger-links-list");
     
-    console.log("=> header");
+    if(burgerLinksContainer.clientHeight === 0) {
+        burgerLinksContainer.style.height = burgerLinksList.clientHeight + px;
+    } else {
+        burgerLinksContainer.style.height = 0 + px;
+    }
 }
 
