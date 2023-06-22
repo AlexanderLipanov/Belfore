0f40628cd1c27df5e999a1ff4006239696c3ee02
Alexandr <lipanov162@gmail.com>
2023-06-22 22:58:55 +0300

    bug fix

--- js/menu.js
+++ js/menu.js
@@ -23,14 +23,15 @@ window.onload = function () {
     });
 
     let b = document.getElementById("header");
-    let h = b === undefined || b === null ? 65 : b.offsetHeight;
+    let h = b === undefined || b === null ? 44.39 : b.offsetHeight;
+    console.log("h", h);
     function onScroll() {
 
         let scrollTop = window.pageYOffset;
         let sectionHome = document.getElementById("section-home");
         
         if(sectionHome === null) {return;}
-        sectionHome.style.height = window.screen.height - scrollTop - h + px;
+        sectionHome.style.height = window.screen.height - scrollTop - h + px;       
     } 
 }
 
